f25b0daa74a25c5b316dd90d1fdd8512b2addadc
saturator22 <f.szczeski@gmail.com>
2018-10-23 12:22:17 +0200

    Implement console version of interacting with app

--- timers/src/main/com/codecool/Main.java
+++ timers/src/main/com/codecool/Main.java
@@ -1,4 +1,40 @@
 package main.com.codecool;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        boolean isWorking = true;
+        List<Timer> timerList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        String[] parsedCommand;
+
+        while(isWorking) {
+            System.out.print("Command ?: ");
+            command = scanner.nextLine();
+            parsedCommand = command.split(" ");
+
+            if(parsedCommand[0].equals("start")) {
+                Timer timer = new Timer((parsedCommand[1]));
+                timer.start();
+                timerList.add(timer);
+
+            } else if(parsedCommand[0].equals("check")) {
+                for(Timer timer: timerList) {
+                    System.out.println(timer.toString());
+                }
+
+            } else if(parsedCommand[0].equals("exit")) {
+                for(Timer timer: timerList) {
+                    timer.interrupt();
+                }
+                isWorking = false;
+            }
+        }
+        System.exit(0);
+    }
 }
