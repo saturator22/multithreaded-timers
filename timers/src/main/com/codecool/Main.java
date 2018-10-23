3131c62781d7cde9883339f951446d110b55e2c9
saturator22 <f.szczeski@gmail.com>
2018-10-23 14:34:17 +0200

    Refactor

--- timers/src/main/com/codecool/Main.java
+++ timers/src/main/com/codecool/Main.java
@@ -7,34 +7,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        boolean isWorking = true;
-        List<Timer> timerList = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        String command;
-        String[] parsedCommand;
-
-        while(isWorking) {
-            System.out.print("Command ?: ");
-            command = scanner.nextLine();
-            parsedCommand = command.split(" ");
-
-            if(parsedCommand[0].equals("start")) {
-                Timer timer = new Timer((parsedCommand[1]));
-                timer.start();
-                timerList.add(timer);
-
-            } else if(parsedCommand[0].equals("check")) {
-                for(Timer timer: timerList) {
-                    System.out.println(timer.toString());
-                }
-
-            } else if(parsedCommand[0].equals("exit")) {
-                for(Timer timer: timerList) {
-                    timer.interrupt();
-                }
-                isWorking = false;
-            }
-        }
+        Application.run();
         System.exit(0);
     }
 }
